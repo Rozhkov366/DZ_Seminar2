32947ccc8c52b9a0e80c7c6c200ceec0e6ec01e6
Rozhkov366 <osipova_g@mail.ru>
2022-11-23 22:51:57 +0300

    добавил задачу1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 10: Напишите программу, которая принимает на
+ вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetRandomNumber(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
+int GetFirstLastNumber (int RandomNumber)
+{
+    int number2= (RandomNumber /10) % 10;
+        return number2;
+   }
+int RandomNumber = GetRandomNumber(100, 999);
+int number = GetFirstLastNumber(RandomNumber);
+Console.WriteLine (RandomNumber);
+Console.Write (number);
